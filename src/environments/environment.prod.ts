5a79238a912fdb504a109f302f7d0c14cfa0859c
ismaelsky1 <ismael.sky1@hotmail.com>
2021-01-07 20:55:31 -0300

    env digOceas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //api: 'https://smartpsi-api-rqeid.ondigitalocean.app',
-  api: 'http://54.163.84.209:3333',
+  api: 'https://smartpsi-api-rqeid.ondigitalocean.app',
+  //api: 'http://54.163.84.209:3333',
   files: 'https://smartpsi.s3.amazonaws.com/'
 };
