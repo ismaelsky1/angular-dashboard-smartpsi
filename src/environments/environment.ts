0680191dc931cf1d78d78134da48c43ee69d6c9e
ismaelsky1 <ismael.sky1@hotmail.com>
2021-01-15 22:41:36 -0300

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
   //api: 'https://smartpsi-api-rqeid.ondigitalocean.app',
   production: false,
   api: 'http://localhost:3333',
+  //api: 'http://ec2-54-163-84-209.compute-1.amazonaws.com:3333',
+
   files: 'https://smartpsi.s3.amazonaws.com/'
 };
